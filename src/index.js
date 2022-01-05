9b86d9a75d98c4fd14474f8f236a064bc5ca3efe
Aliaksandr <al_brain8@tut.by>
2022-01-05 19:35:58 +0300

    docs: create function 1000/1000

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    let arr = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve','thirteen','fourteen','fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arr10 = ['0','ten','twenty', 'thirty','forty','fifty', 'sixty','seventy','eighty','ninety']
+    
+    if(n<20){
+      return arr[n]
+    }else if(n<100){
+      n=n.toString().split('').map(Number)
+      if(n[1]===0){
+        return arr10[n[0]]
+      }else{
+        return arr10[n[0]]+' '+arr[n[1]]
+      }
+    }else if(n>99){
+      n=n.toString().split('').map(Number);
+      if(n[1]+n[2]===0){
+         return arr[n[0]]+" hundred"
+      }else if(n[1]===0){
+       return arr[n[0]]+" hundred "+arr[n[2]] 
+      }else if(n[2]===0){
+        return arr[n[0]]+" hundred "+arr10[n[1]]
+      }else if(n[1]+""+n[2]>20){
+        return arr[n[0]]+" hundred "+arr10[n[1]]+" "+arr[n[2]]
+      }else if(n[1]+""+n[2]<20){
+        return arr[n[0]]+" hundred "+arr[n[1]+""+n[2]]
+      }
+     
+    }
 }
